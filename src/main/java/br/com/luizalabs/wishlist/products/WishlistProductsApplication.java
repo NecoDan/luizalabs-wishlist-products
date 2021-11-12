d89970c9df8f8e70dbc976045bea33ffe503d28c
Daniel Santos <neco.daniel@gmail.com>
2021-11-11 22:54:07 -0300

    inicializando e carregando repositorio projeto

---
+++ src/main/java/br/com/luizalabs/wishlist/products/WishlistProductsApplication.java
@@ -0,0 +1,13 @@
+package br.com.luizalabs.wishlist.products;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WishlistProductsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WishlistProductsApplication.class, args);
+    }
+
+}
