f9a3eb965c753a5e2f8869bbc832bdf15b22e166
Daniel Santos <neco.daniel@gmail.com>
2021-11-15 12:06:48 -0300

    configs implementacao inicial

--- src/main/java/br/com/luizalabs/wishlist/products/properties/TransactionProperties.java
+++ src/main/java/br/com/luizalabs/wishlist/products/properties/TransactionProperties.java
@@ -1,4 +1,12 @@
 package br.com.luizalabs.wishlist.products.properties;
 
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+@Data
+@Configuration
+@ConfigurationProperties(prefix = "transaction")
 public class TransactionProperties {
+    private long limitMaxProductsWishlist;
 }
